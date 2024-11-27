5c2e1358a30b216640144291dd7c0dbd557f852a
PiDinosaur2804 <ngodinhluyennht@gmail.com>
2024-11-28 02:02:57 +0700

    update

--- trainers/EoETrainer.py
+++ trainers/EoETrainer.py
@@ -89,6 +89,9 @@ class EoETrainer(BaseTrainer):
             
             print(model.num_labels)
             
+            
+            print(model.expert_distribution['class_mean'])
+            print(model.expert_distribution['accumulate_cov_shared'])
             baseHidden = BaseHidden(model.num_labels, model.expert_distribution['class_mean'], model.expert_distribution['accumulate_cov_shared'])
             hidden_data = baseHidden.generate_hidden_data()
             hidden_dataset = BaseDataset(hidden_data)  
